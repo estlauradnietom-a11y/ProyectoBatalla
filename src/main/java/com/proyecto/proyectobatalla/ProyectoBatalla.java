1c2a286134318d10664264613ddb03902be07c8c
Laura <est.laurad.nietom@unimilitar.edu.co>
2026-03-17 08:21:50 -0500

    inicializacion de repositorio

---
+++ src/main/java/com/proyecto/proyectobatalla/ProyectoBatalla.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.proyectobatalla;
+
+/**
+ *
+ * @author Admin
+ */
+public class ProyectoBatalla {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
