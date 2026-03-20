bbfd52a476e2d1351daa84897c2b2c88c109757c
Laura <est.laurad.nietom@unimilitar.edu.co>
2026-03-19 21:56:14 -0500

    Version 1, creacion de clases con estructura aplicacion de librerias (hay errores)

---
+++ ProyectoBatalla1/src/main/java/com/proyecto/proyectobatalla1/ProyectoBatalla1.java
@@ -0,0 +1,23 @@
+
+package com.proyecto.proyectobatalla1;
+
+/**
+ *
+ * @author laura
+ */
+public class ProyectoBatalla1 {
+
+    
+
+    public static void main(String[] args) {
+
+        Juego juego = new Juego();
+
+        juego.agregarJugador();
+        juego.agregarJugador();
+
+    }
+
+}
+    
+ 
